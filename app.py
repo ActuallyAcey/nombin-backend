4056c5171167a46053adcb64752a85af5b7f6d2d
Acey <ActuallyAcey@gmail.com>
2019-05-03 22:36:30 +0530

    Working POST and GET for sample data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_ngrok import run_with_ngrok
 from flask_cors import CORS
 
@@ -6,40 +6,57 @@ app = Flask(__name__)
 run_with_ngrok(app)
 CORS(app)
 
+previous_post_key = 0
 
-testdata1 = {
+uploaded_noms = [
+        {
+        'key': 100,
         'title': 'First Title',
         'text': 'Magna quis ad esse pariatur sunt veniam cupidatat. Ullamco exercitation ad incididunt labore sint mollit ullamco sunt sint do reprehenderit. Cupidatat ea deserunt anim et commodo in adipisicing laborum laborum duis irure aliqua sit. Lorem amet laboris proident velit Lorem magna officia non excepteur dolor. Consectetur adipisicing deserunt sint laborum sunt nulla ullamco non duis.', 
         'tags': ['tag1', 'tag2', 'ugly bastard ( ͡° ͜ʖ ͡°)'],
         'is_private': False
-        }
+        },
 
-testdata2 = {
+        {
+        'key': 101,
         'title': 'Second Title',
         'text': 'Consequat commodo eu culpa sit cillum magna pariatur occaecat proident proident. Pariatur adipisicing non do velit labore. Adipisicing nostrud ipsum non ullamco sit consequat sunt deserunt dolore ex ad officia. Pariatur cupidatat est nulla quis nostrud irure amet laborum minim deserunt ad ullamco officia incididunt. Aliqua ullamco id eu ea consequat incididunt aute labore qui dolor qui.',
         'tags': ['tag3', 'tag4', 'STILL ugly bastard ( ͡° ͜ʖ ͡°)'],
         'is_private': True
         }
-
-incoming_data = []
+]
 
 @app.route('/')
 def index():
     return "Hello, World!"
 
 
-@app.route('/api/test/<id>', methods = ['GET'])
+@app.route('/<id>', methods = ['GET'])
 def test(id):
+        if (id == 'favicon.ico'):
+                return jsonify({"message": "favicon"}) 
 
-        if (id == 'set1'):
-                return jsonify(testdata1)
-        elif (id == 'set2'):
-                return jsonify(testdata2)
-
-
-@app.route('/api/test/', methods = ['POST'])
-def push_data():
-        pass
+        check_id = int(id)
+        for nom in uploaded_noms:
+                if nom['key'] == check_id:
+                        return jsonify(nom)
         
+        return jsonify({"message": "No post found"})
+
+
+@app.route('/new', methods = ['POST'])
+def push_data():
+        data = request.get_json()
+
+        post = {'key': previous_post_key + 1, 
+                'title': data['title'],
+                'text': data['text'],
+                'tags': data['tags'],
+                'is_private': data['is_private']}
+
+        uploaded_noms.append(post)
+        return jsonify({'key': previous_post_key + 1})
+
+
 if __name__ == '__main__':
     app.run()
