86bd104c0bfac1611e415540ac7c0a470fb2c02e
Acey <ActuallyAcey@gmail.com>
2019-05-05 16:30:10 +0530

    Moved test code to decent test module lol

--- app.py
+++ app.py
@@ -1,31 +1,11 @@
 from flask import Flask, jsonify, request
-from flask_ngrok import run_with_ngrok
+from test import run_with_ngrok
 from flask_cors import CORS
 
 app = Flask(__name__)
-run_with_ngrok(app)
-CORS(app)
-app.url_map.strict_slashes = False
-
-previous_post_key = 0
-
-uploaded_noms = [
-        {
-        'key': 100,
-        'title': 'First Title',
-        'text': 'Magna quis ad esse pariatur sunt veniam cupidatat. Ullamco exercitation ad incididunt labore sint mollit ullamco sunt sint do reprehenderit. Cupidatat ea deserunt anim et commodo in adipisicing laborum laborum duis irure aliqua sit. Lorem amet laboris proident velit Lorem magna officia non excepteur dolor. Consectetur adipisicing deserunt sint laborum sunt nulla ullamco non duis.', 
-        'tags': ['tag1', 'tag2', 'ugly bastard ( ͡° ͜ʖ ͡°)'],
-        'is_private': False
-        },
-
-        {
-        'key': 101,
-        'title': 'Second Title',
-        'text': 'Consequat commodo eu culpa sit cillum magna pariatur occaecat proident proident. Pariatur adipisicing non do velit labore. Adipisicing nostrud ipsum non ullamco sit consequat sunt deserunt dolore ex ad officia. Pariatur cupidatat est nulla quis nostrud irure amet laborum minim deserunt ad ullamco officia incididunt. Aliqua ullamco id eu ea consequat incididunt aute labore qui dolor qui.',
-        'tags': ['tag3', 'tag4', 'STILL ugly bastard ( ͡° ͜ʖ ͡°)'],
-        'is_private': True
-        }
-]
+run_with_ngrok(app)                # TODO: Testing
+CORS(app)                          # CORS hates APIs being used like they normally are
+app.url_map.strict_slashes = False # Fixes "/new/" vs "/new" error
 
 @app.route('/')
 def index():
@@ -34,33 +14,15 @@ def index():
 
 @app.route('/<id>', methods = ['GET'])
 def test(id):
-        if (id == 'favicon.ico'):
-                return jsonify({"message": "favicon"}) 
-
-        check_id = int(id)
-        for nom in uploaded_noms:
-                if nom['key'] == check_id:
-                        return jsonify(nom)
-        
-        return jsonify({"message": "No post found"})
+        return jsonify(test.get(id)) 
 
 
 @app.route('/new', methods = ['POST'])
 def push_data():
-        global previous_post_key
         data = request.get_json()
+        key = test.add(data)
 
-        post = {'key': previous_post_key + 1, 
-                'title': data['title'],
-                'text': data['text'],
-                'tags': data['tags'],
-                'is_private': data['is_private']}
-
-
-        uploaded_noms.append(post)
-        previous_post_key += 1
-
-        return jsonify({'key': previous_post_key})
+        return jsonify({'key': key})
 
 
 if __name__ == '__main__':
