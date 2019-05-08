1b5fbcdbc8a034ad4c75e3d24635019a692708b7
Acey <ActuallyAcey@gmail.com>
2019-05-09 01:23:57 +0530

    Bits of restructuring main file

--- app.py
+++ app.py
@@ -3,34 +3,37 @@ from flask_cors import CORS
 
 import os
 
-from test import run_with_ngrok		#TODO: Remove testing
+from test import run_with_ngrok		#TODO: Remove testing code
 import test
 
 app = Flask(__name__)
-run_with_ngrok(app)                # TODO: Remove Testing
-CORS(app)                          # CORS hates APIs being used like they normally are
-app.url_map.strict_slashes = False # Fixes "/new/" vs "/new" error
 
-@app.route('/favicon.ico')
-def favicon():
-    	return send_from_directory(os.path.join(app.root_path, 'static'),
-                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+run_with_ngrok(app)  				#TODO: Remove more testing code
 
-@app.route('/')
-def index():
-	return "Hello, World!"
+CORS(app)                          	# CORS hates APIs being used like they normally are
+app.url_map.strict_slashes = False 	# Fixes "/new/" vs "/new" error
 
 
 @app.route('/<id>', methods = ['GET'])
 def get_data(id):
 	return jsonify(test.get(id))
-	# TODO: Get from Database
+
 
 @app.route('/new', methods = ['POST'])
 def push_data():
     	key = test.add(request.get_json())
     	return jsonify({'key': key})
 
+
+@app.route('/')
+def index():
+	return "Hello, World!"
+
+
+@app.route('/favicon.ico')
+def favicon():
+    	return send_from_directory(os.path.join(app.root_path, 'static'),
+                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     	app.run()
