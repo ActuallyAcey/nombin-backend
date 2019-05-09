63af0e0e960088d3b2a5b9ea0ab41d08bbecf1fc
Acey <ActuallyAcey@gmail.com>
2019-05-10 02:55:48 +0530

    Implemented databases via cloud connection (woo!)

--- app.py
+++ app.py
@@ -1,28 +1,55 @@
 from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
+from flask_pymongo import PyMongo
+from flask_ngrok import run_with_ngrok 
 
 import os
 
-from test import run_with_ngrok		#TODO: Remove testing code
-import test
+previous_post_key = 0
 
 app = Flask(__name__)
 
 run_with_ngrok(app)  				#TODO: Remove more testing code
 
-CORS(app)                          	# CORS hates APIs being used like they normally are
+# CONFIGURATIONS
+                          	# CORS hates APIs being used like they normally are I guess?
 app.url_map.strict_slashes = False 	# Fixes "/new/" vs "/new" error
+app.config['MONGO_DBNAME'] = 'NomBinDB'
+app.config['MONGO_URI'] = 'mongodb+srv://nom:nomnomnom@nombindb-i0qby.mongodb.net/test?retryWrites=true'
 
+mongo = PyMongo(app)
+CORS(app)
 
 @app.route('/<id>', methods = ['GET'])
 def get_data(id):
-	return jsonify(test.get(id))
+
+	check_id = int(id)
+
+	noms = mongo.db.nom_list
+	fetched_nom = noms.find_one({'key' : check_id})
+	
+	if "_id" in fetched_nom:
+		print ("Detected id, stripping off.")
+		del fetched_nom["_id"]
+
+	return jsonify(fetched_nom)
 
 
 @app.route('/new', methods = ['POST'])
 def push_data():
-    	key = test.add(request.get_json())
-    	return jsonify({'key': key})
+	global previous_post_key
+	data = request.get_json()
+
+	new_nom = {	'key' : previous_post_key + 1,
+				'title': data['title'],
+        		'text': data['text'],
+        		'tags': data['tags'],
+        		'is_private': data['is_private']}
+
+	noms = mongo.db.nom_list
+	noms.insert(new_nom)
+	previous_post_key += 1
+	return 'Added successfully!'
 
 
 @app.route('/')
